66cef5aab5d59588637e79dfc8379a0d434e263c
Quantum Snowball <quantum.snowball@gmail.com>
2021-08-07 04:53:52 +0800

    feature: extracted all valid search results title

--- opensubtitles_dl/manager.py
+++ opensubtitles_dl/manager.py
@@ -3,7 +3,7 @@ from opensubtitles_dl.search import search
 
 
 def cli():
-    print(search('nomadland', lang='eng'))
+    search('nomadland', lang='eng')
 
 
 if __name__ == '__main__':
--- opensubtitles_dl/search.py
+++ opensubtitles_dl/search.py
@@ -9,6 +9,16 @@ def make_url(*words, lang):
     return url
 
 
+def extract_innerhtml(ct):
+    for i in range(1, len(ct)-1):
+        if str(ct[i-1]) == '<br/>' and str(ct[i+1]) == '<br/>':
+            try:
+                return ct[i].text.strip()
+            except AttributeError:
+                return str(ct[i])
+    return ''
+
+
 def search(*words, lang):
     url = make_url(*words, lang=lang)
     session = requests.Session()
@@ -16,9 +26,12 @@ def search(*words, lang):
     resp = session.get(url)
     with open('html.html', 'wb') as f:
         f.write(resp.content)
-    soup = BeautifulSoup(resp.content)
+    soup = BeautifulSoup(resp.content, 'lxml')
     table = soup.select_one('#search_results')
-
-    
+    trs = table.select('tr[id^=name]')
+    for i, tr in enumerate(trs):
+        td = tr.select_one('td[id^=main]')
+        title = extract_innerhtml(td.contents)
+        print(f'{i+1: >2}. {title}')
 
     return url
