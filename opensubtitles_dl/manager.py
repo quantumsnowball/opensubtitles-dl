a91b03a4650f103f120b5aa057c088c588d72505
Quantum Snowball <quantum.snowball@gmail.com>
2021-08-07 03:23:17 +0800

    feature: make_url

--- opensubtitles_dl/manager.py
+++ opensubtitles_dl/manager.py
@@ -1,5 +1,10 @@
 import click
+from opensubtitles_dl.search import search
 
 
 def cli():
-    print('hello world')
+    print(search('hello', 'world', lang='abc'))
+
+
+if __name__ == '__main__':
+    cli()
