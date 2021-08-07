d4dc8f6173564ee4017e8df54949663abab56cdd
Quantum Snowball <quantum.snowball@gmail.com>
2021-08-07 22:41:21 +0800

    debug: setuptools added packages

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='opensubtitles-dl',
       version='1.0',
       description='opensubtitles-dl',
+      packages=['opensubtitles_dl', ],
       entry_points={
           'console_scripts': [
               'opensubtitles-dl=opensubtitles_dl.manager:cli',
