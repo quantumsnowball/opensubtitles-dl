8c4c67f71554d8f49c4e964529f50b760c6235e3
Quantum Snowball <quantum.snowball@gmail.com>
2022-05-25 19:54:38 +0800

    more setup configs

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(name='opensubtitles-dl',
-      version='1.0.0',
-      description='opensubtitles-dl',
       packages=['opensubtitles_dl', ],
+      version='1.0.0',
+      description='A simple cli tool to download from opensubtitles.org',
+      author='Quantum Snowball',
+      author_email='quantum.snowball@gmail.com',
+      url='https://github.com/quantumsnowball/opensubtitles-dl',
+      keywords=['subtitles', 'opensubtitles', ],
+      python_requires='>=3.6',
       install_requires=['click', 'requests', ],
+      classifiers=[
+          'Programming Language :: Python 3.6'
+      ],
       entry_points={
           'console_scripts': [
               'opensubtitles-dl=opensubtitles_dl.manager:cli',
