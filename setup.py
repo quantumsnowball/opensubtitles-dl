996af536dcf153150389a1def170336c2b06ccd3
Quantum Snowball <quantum.snowball@gmail.com>
2022-05-25 19:37:49 +0800

    specify dependencies

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(name='opensubtitles-dl',
-      version='1.0',
+      version='1.0.0',
       description='opensubtitles-dl',
       packages=['opensubtitles_dl', ],
+      install_requires=['click', 'requests', ],
       entry_points={
           'console_scripts': [
               'opensubtitles-dl=opensubtitles_dl.manager:cli',
