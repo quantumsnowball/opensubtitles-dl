6c40360018e72cff0d197d3d781052c47d092b08
Quantum Snowball <quantum.snowball@gmail.com>
2022-05-27 01:32:47 +0800

    test: language flags

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,8 +15,7 @@ def test_cli_no_args(script_runner):
     assert result.stdout.split('\n')[0].startswith('Usage:')
 
 
-@pytest.mark.parametrize('keywords', [
-    (kw, ) for kw in ('spider man', 'iron man', 'captain america')])
+@pytest.mark.parametrize('keywords', ('spider man', 'iron man', 'captain america'))
 def test_cli_with_search_keywords(keywords, script_runner):
     with mock.patch.object(builtins, 'input', lambda _: 'q'):
         result = script_runner.run(f'opensubtitles-dl', keywords)
@@ -25,8 +24,8 @@ def test_cli_with_search_keywords(keywords, script_runner):
             'Please select subtitle by id:')
 
 
-@pytest.mark.parametrize('n', [n for n in (5, 10, 20)])
-def test_cli_with_n_limit(n, script_runner):
+@pytest.mark.parametrize('n', (5, 10, 20))
+def test_cli_with_limit_flag(n, script_runner):
     with mock.patch.object(builtins, 'input', lambda _: 'q'):
         result = script_runner.run(
             f'opensubtitles-dl', f'-n {n}', 'spider man')
@@ -34,3 +33,16 @@ def test_cli_with_n_limit(n, script_runner):
         assert result.stdout.split('\n')[0].startswith(
             'Please select subtitle by id:')
         assert len(result.stdout.split('\n')) == n + 2
+
+
+@pytest.mark.parametrize('l', ('chi', 'spn', 'eng', 'hin', 'jpn', 'kor'))
+def test_cli_with_language_flag(l, script_runner):
+    with mock.patch.object(builtins, 'input', lambda _: 'q'):
+        result = script_runner.run(
+            f'opensubtitles-dl', f'-l {l}', 'spider man')
+        assert result.success
+        assert result.stdout.split('\n')[0].startswith(
+            'Please select subtitle by id:')
+        entries = result.stdout.split('\n')[1:-1]
+        if len(entries) >= 0:
+            assert all([l in entry for entry in entries])
